ad2476e97f86bb2ddeac5455c5af36dc8f9aa01b
jaeyu <88123634+jaeyulove@users.noreply.github.com>
2024-04-19 18:47:29 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 1.54 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void triangle(int n){
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=i; j++){
+            printf("*");
+        }
+              printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    triangle(n);
+
+    return 0;
+}
