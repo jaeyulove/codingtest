92c9d4cd2319773fbb4229870cc0bb6a8cca8429
jaeyu <88123634+jaeyulove@users.noreply.github.com>
2024-05-05 14:55:15 +0900

    [level 0] Title: 종이 자르기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.c
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.c
@@ -2,17 +2,14 @@
 #include <stdbool.h>
 
 int solution(int M, int N) {
-    int width = (M-1)*N;
-    int length = (N-1); 
     
-    return width + length; 
 }
 
 int main(void){
     int M = 2;
     int N = 2;
     
-    int result = solution(M,N);
+    int result = soluiton(M,N);
     printf("%d", result);
     return 0; 
 }
