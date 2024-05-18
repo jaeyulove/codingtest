3bc0c4665f9660537073002d79b0b450a51f62d2
jaeyu <88123634+jaeyulove@users.noreply.github.com>
2024-05-18 17:14:06 +0900

    [level 0] Title: n 번째 원소부터, Time: 0.01 ms, Memory: 4.06 MB -BaekjoonHub

---
+++ 프로그래머스/0/181892. n 번째 원소부터/n 번째 원소부터.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// num_list_len은 배열 num_list의 길이입니다.
+int* solution(int num_list[], size_t num_list_len, int n) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    
+    int* answer = (int*)malloc(sizeof(int)*(num_list_len+1)-n);
+    int index = 0;
+    
+    if(answer == NULL){
+        printf("memory allocation failed");
+        return NULL; 
+    }
+    
+    for(int i=n-1; i<num_list_len; i++){
+        answer[index++] = num_list[i]; 
+    }    
+    return answer;
+}
+
+int main(void){
+    int num_list[] = {2,1,6}; 
+    size_t num_list_len = sizeof(num_list)/sizeof(num_list[0]); 
+    int n = 3;
+    int* result = solution(num_list,num_list_len,n); 
+    
+    if(result != NULL){
+        for(int i=0; i<(num_list_len+1)-n; i++){
+        printf("%d", result);
+      }
+        free(result);
+    } else {
+        printf("memory allocation failed \n"); 
+    }
+    return 0; 
+}
