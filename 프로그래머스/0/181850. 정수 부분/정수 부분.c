7203eb622d52d4df688159226fe1a0b8ceef6f8d
jaeyu <88123634+jaeyulove@users.noreply.github.com>
2024-08-17 11:02:45 +0900

    [level 0] Title: 정수 부분, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(double flo) {
+    int answer = 0;
+    answer= flo;
+    return answer;
+}
+
+int main(void){
+    double flo = 1.42;
+    int result = solution(flo);
+    printf("%d\n", result);
+    return 0; 
+}
